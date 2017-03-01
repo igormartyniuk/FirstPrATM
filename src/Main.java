ee741b064033b1803c331c98463f6711cc7e5825
Kanibal <Kanibal@DESKTOP-J92RFN7>
2017-03-01 15:12:35 +0200

    pass 3

--- src/Main.java
+++ src/Main.java
@@ -15,145 +15,167 @@ public class Main {
 				while(true){
 					System.out.println("enter the password");
 					
-					if(scanner.hasNextInt()){
-						p = scanner.nextInt();
 					
-					if(p!=pass){
-						System.out.println("Invalid password! Please try again.");
-						System.out.println("__________________________");
-						break;
-					}
-					else if(p==pass){
-						System.out.println("the password is correct");
-						System.out.println("__________________________");
+					for (int i = 0; i < 3; i++) {
 						
-						while(true){
-							System.out.println("enter 1 for check balance");
-							System.out.println("enter 2 for a credit");
-							System.out.println("enter 3 for add into a balance");
-							System.out.println("enter 2 for Exit");
+						if(scanner.hasNextInt()){
+							p = scanner.nextInt();
+						
 							
-							
-							
-							
-//							if(!scanner.hasNextInt()){
-//								System.out.println("incorrect");
-//								break;
-//							}
-//							else if(scanner.hasNextInt()){
-								int choise = scanner.nextInt();
-						switch (choise) {
-						case 1:{
-							System.out.println("__________________________");
-							System.out.println("your current balance = "+balance);
-							System.out.println("Enter 0 - Exit  1 - Continue");
-							System.out.println("__________________________");
-							
-							int or = scanner.nextInt();
-							if(or==0){
+							if(p!=pass){
+								System.out.println("Invalid password! Please try again.");
 								System.out.println("__________________________");
-								System.out.println("Good bye!");
-								return;
 							}
-							else if(or==1){
+						
+							else if (p==pass){
+								System.out.println("the password is correct");
+								System.out.println("__________________________");
 								
-							}
-							break;
-						
-						}case 2:{
-							System.out.println("__________________________");
-							System.out.println("How much you want to take off?");
-							int credit = scanner.nextInt();
-							if(credit<0){
-								System.out.println(" ");
-								System.out.println("sum can not be negative");
-							}
-							else if(balance<credit){
-								System.out.println("__________________________");
-								System.out.println("insufficient funds");
-							}
-							else if(balance>=credit){
-							balance = balance - credit;
-							}
-							
-							for (int i = 0; i < a.length; i++) {
-								if(credit>=a[i]){
-									System.out.println("number of "+a[i]+" - "+credit/a[i]);
-									credit = credit % a[i];
+								while(true){
+									System.out.println("enter 1 for check balance");
+									System.out.println("enter 2 for a credit");
+									System.out.println("enter 3 for add into a balance");
+									System.out.println("enter 2 for Exit");
+									
+										
+									int choise = scanner.nextInt();
+									switch (choise) {
+										case 1:{
+											System.out.println("__________________________");
+											System.out.println("your current balance = "+balance);
+											System.out.println("Enter 0 - Exit  1 - Continue");
+											System.out.println("__________________________");
+											
+											int or = scanner.nextInt();
+											if(or==0){
+												System.out.println("__________________________");
+												System.out.println("Good bye!");
+												return;
+											}
+											else if(or==1){
+												
+											}
+											break;
+										
+										}case 2:{
+											System.out.println("__________________________");
+											System.out.println("How much you want to take off?");
+											int credit = scanner.nextInt();
+											if(credit<0){
+												System.out.println(" ");
+												System.out.println("sum can not be negative");
+											}
+											
+											else if(balance<credit){
+												System.out.println("__________________________");
+												System.out.println("insufficient funds");
+												System.out.println("__________________________");
+												System.out.println("");
+												break;
+											}
+											
+											else if(balance>=credit){
+											balance = balance - credit;
+											}
+											
+											for (int j = 0; i < a.length; j++) {
+												if(credit>=a[j]){
+													System.out.println("number of "+a[j]+" - "+credit/a[j]);
+													credit = credit % a[j];
+												}
+											}
+																								
+											System.out.println("__________________________");
+											System.out.println("your current balance = "+balance);
+											System.out.println("Enter 0 - Exit  1 - Continue");
+											System.out.println("__________________________");
+											
+											int or = scanner.nextInt();
+											
+											if(or==0){
+												System.out.println("__________________________");
+												System.out.println("Good bye!");
+												return;
+											}
+											else if(or==1){
+											break;
+											}
+										
+										
+										}case 3:{
+											System.out.println("__________________________");
+											System.out.println("Do you want to replenish your balance?");
+											
+											int add = scanner.nextInt();
+											
+											if(add<0){
+												System.out.println(" ");
+												System.out.println("sum can not be negative");
+												System.out.println("__________________________");
+											}
+											
+											else{
+											balance = balance + add;
+											}
+											
+											System.out.println("__________________________");
+											System.out.println("your current balance = "+balance);
+											System.out.println("Enter 0 - Exit  1 - Continue");
+											System.out.println("__________________________");
+											
+											int or = scanner.nextInt();
+											
+											if(or==0){
+												System.out.println("__________________________");
+												System.out.println("Good bye!");
+												return;
+											}
+											
+											else if(or==1){
+											break;
+											}
+											
+										}case 4:{
+											if(choise==4){
+												System.out.println("__________________________");
+											System.out.println("Exit");
+											System.out.println("Enter 0 - Exit  1 - Continue");
+											
+											int or = scanner.nextInt();
+											
+											if(or==0){
+												System.out.println("__________________________");
+												System.out.println("Good bye!");
+												return;
+											}
+											
+											else if(or==1){
+											break;
+											}
+											
+											}
+										}
+										default:{
+											System.out.println("__________________________");
+											System.out.println("Not right !, try again");
+											System.out.println("__________________________");
+											break;
+										}
+									}
 								}
-							}
-																				
-							
-							System.out.println("__________________________");
-							System.out.println("your current balance = "+balance);
-							System.out.println("Enter 0 - Exit  1 - Continue");
-							System.out.println("__________________________");
-							int or = scanner.nextInt();
-							
-							
-							if(or==0){
-								System.out.println("__________________________");
-								System.out.println("Good bye!");
-								return;
-							}
-							else if(or==1){
-							break;
+									
 							}
 						
+					}
+					
 						
-						}case 3:{
-							System.out.println("__________________________");
-							System.out.println("Do you want to replenish your balance?");
-							int add = scanner.nextInt();
-							if(add<0){
-								System.out.println(" ");
-								System.out.println("sum can not be negative");
-								System.out.println("__________________________");
-							}else
-							balance = balance + add;
-							System.out.println("__________________________");
-							System.out.println("your current balance = "+balance);
-							System.out.println("Enter 0 - Exit  1 - Continue");
-							System.out.println("__________________________");
-							int or = scanner.nextInt();
-							if(or==0){
-								System.out.println("__________________________");
-								System.out.println("Good bye!");
-								return;
-							}
-							else if(or==1){
-							break;
-							}
-						}case 4:{
-							if(choise==4){
-								System.out.println("__________________________");
-							System.out.println("Exit");
-							System.out.println("Enter 0 - Exit  1 - Continue");
-							int or = scanner.nextInt();
-							
-							if(or==0){
-								System.out.println("__________________________");
-								System.out.println("Good bye!");
-								return;
-							}
-							else if(or==1){
-							break;
-							}
-							}
-						}
-						default:{
-							System.out.println("__________________________");
-							System.out.println("Not right !, try again");
-							System.out.println("__________________________");
-							break;
-						}
-						}
-						}
-							
-					}	
 				}
+					System.out.println("");
+					System.out.println("You are blocked");
+					
 					scanner.close();
-
+					
+					break;
 					
 				}
 
